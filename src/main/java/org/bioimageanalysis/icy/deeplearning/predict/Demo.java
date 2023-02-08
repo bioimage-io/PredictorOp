db57d98c142e40617ff10c223ff52c989e0e2279
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-02-08 17:10:00 +0100

    Flesh the demo a bit.

--- src/main/java/org/bioimageanalysis/icy/deeplearning/predict/Demo.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/predict/Demo.java
@@ -6,16 +6,67 @@ import java.io.FileNotFoundException;
 import org.bioimageanalysis.icy.deeplearning.engine.EngineInfo;
 import org.bioimageanalysis.icy.deeplearning.model.Model;
 
+import ij.IJ;
+import ij.ImagePlus;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.util.Util;
+
 public class Demo
 {
 
-	public static void main( final String[] args )
+	public static < I extends NumericType< I > & NativeType< I >, O extends NumericType< O > & NativeType< O > > void main( final String[] args )
 	{
-		final Model model = loadModel();
 		try
 		{
+			/*
+			 * Load the input image.
+			 */
+			final String imgPath = "/Users/tinevez/Desktop/DemoModelRunner/sample_input_0.tif";
+			final ImagePlus imp = IJ.openImage( imgPath );
+			final Img< I > img = ImagePlusAdapter.wrap( imp );
+			// Could be determined from the input file.
+			final String inputAxes = "xyz";
+			System.out.println( "Image loaded: " + img + " with axes: " + inputAxes + " and type: " + img.firstElement().getClass().getSimpleName() );
+
+			/*
+			 * Load the model.
+			 */
+			final Model model = loadModel();
+			System.out.println( "Model loaded: " + model ); // DEBUG
+
+			/*
+			 * Load the specs.
+			 */
 			final ModelSpec spec = ModelSpec.from( model );
-			System.out.println( spec ); // DEBUG
+			System.out.println( "Model specs loaded. Output axes: " + spec.outputAxes + " - Output data type: " + spec.outputType().getClass().getSimpleName() );
+
+			/*
+			 * Reshape the input to match the specs.
+			 */
+			final RandomAccessibleInterval< I > input = AxesMatcher.matchAxes( spec.outputAxes, inputAxes, img );
+			System.out.println( "Input reshaped: " + input );
+
+			/*
+			 * Prepare holder for results.
+			 */
+			final ShapeMath shapeMath = new ShapeMath( spec );
+			final Interval outputInterval = shapeMath.getOutputInterval( input );
+			@SuppressWarnings( "unchecked" )
+			final O outputType = ( O ) spec.outputType();
+			final ImgFactory< O > factory = Util.getArrayOrCellImgFactory( outputInterval, outputType );
+			final Img< O > output = factory.create( outputInterval );
+			System.out.println( "Output holder prepared: " + output );
+
+			/*
+			 * Run the model on the input writing in the output image.
+			 */
+
 		}
 		catch ( final FileNotFoundException e )
 		{
